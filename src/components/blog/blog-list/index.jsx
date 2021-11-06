5357d656b10eb5cd2d6caf10aeaf59597abf170e
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-06 16:08:13 +0100

    connecting FE with BE

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -2,16 +2,53 @@ import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
 import posts from "../../../data/posts.json";
+import Loading from "../../../components/loader/index";
+
 export default class BlogList extends Component {
+  state = {
+    loading: true,
+    error: false,
+    blogPosts: [],
+  };
+
+  componentDidMount() {
+    this.handleFetch()
+  }
+
+  handleFetch = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/blogPosts/");
+
+      if (response.ok) {
+        const data = await response.json();
+        this.setState({
+          blogPosts: data,
+          loading: false,
+        });
+      }
+    } catch (error) {
+      this.setState({ loading: false, error: message });
+    }
+  };
+
   render() {
-    return (
-      <Row>
-        {posts.map((post) => (
-          <Col md={4} style={{ marginBottom: 50 }}>
-            <BlogItem key={post.title} {...post} />
-          </Col>
-        ))}
-      </Row>
-    );
+    const { loading, error, blogPosts } = this.state;
+    if (loading) {
+      return <Loading />;
+    } else {
+      if (error) {
+        return <div>{error}</div>;
+      }else{
+        return (
+          <Row>
+            {blogPosts.map((blogPost) => (
+              <Col xs={12} sm={6} md={4} lg={3} key={blogPost.id}>
+                <BlogItem listing={blogPost} />
+              </Col>
+            ))}
+          </Row>
+        );
+      }
+    }
   }
 }
---
+++ src/components/loader/index.jsx
@@ -0,0 +1,19 @@
+import { Spinner } from "react-bootstrap";
+
+export default function loader() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </div>
+  );
+}
